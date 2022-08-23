45ca3eec1b9d417af212119ec583e123a548c1e3
austincomstock <austinjcomstock@gmail.com>
2022-08-23 14:12:24 -0600

    Created & imported Bottom component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Navigation } from "./components/Navigation/Navigation";
 import { VisualMedia } from "./components/VisualMedia/VisualMedia";
 import { About } from "./components/About/About";
 import { Links } from "./components/Links/Links";
+import { Bottom } from "./components/Bottom/Bottom";
 
 function App() {
   return (
@@ -16,6 +17,16 @@ function App() {
       <VisualMedia />
       <About />
       <Links />
+      <Bottom />
+      {/* <footer>
+        <h1>Contact & Connect With Us</h1>
+        <h3>Contact</h3>
+        <p>Phone: 800-000-000</p>
+        <p>Email: articuateauto@mail.placeholder</p>
+        <h3>Connect</h3>
+        <p>Facebook, Instagram</p>
+        <p>Instagram</p>
+      </footer> */}
     </div>
   );
 }
---
+++ src/components/Bottom/Bottom.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Bottom.css";
+
+export const Bottom = () => {
+  return (
+    <div className="Bottom1">
+      <h3 className="BottomH3">Contact & Connect With Us</h3>
+      <h5 className="BottomH5">Contact</h5>
+      <ul>
+        <li>Phone: 800-000-000</li>
+        <li>Email: articuateauto@mail.placeholder</li>
+      </ul>
+      <h5 className="BottomH5">Connect</h5>
+      <ul>
+        <li>Facebook</li>
+        <li>Instagram</li>
+      </ul>
+    </div>
+  );
+};
