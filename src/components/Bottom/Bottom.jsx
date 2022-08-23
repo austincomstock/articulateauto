c54a761f0c9f900548e13c279f7ee5ece4a9e865
austincomstock <austinjcomstock@gmail.com>
2022-08-23 14:45:27 -0600

    Added react-icons

--- src/components/Bottom/Bottom.jsx
+++ src/components/Bottom/Bottom.jsx
@@ -1,19 +1,31 @@
 import React from "react";
 import "./Bottom.css";
 
+// REACT-ICONS IMPORTS
+import { BsFacebook, BsInstagram, BsTelephone } from "react-icons/bs";
+import { MdOutlineMail } from "react-icons/md";
+
 export const Bottom = () => {
   return (
     <div className="Bottom1">
       <h3 className="BottomH3">Contact & Connect With Us</h3>
       <h5 className="BottomH5">Contact</h5>
-      <ul>
-        <li>Phone: 800-000-000</li>
-        <li>Email: articuateauto@mail.placeholder</li>
+      <ul className="BottomUL">
+        <li>
+          <BsTelephone /> 800-000-000
+        </li>
+        <li>
+          <MdOutlineMail /> articulateauto@mail.placeholder
+        </li>
       </ul>
       <h5 className="BottomH5">Connect</h5>
-      <ul>
-        <li>Facebook</li>
-        <li>Instagram</li>
+      <ul className="BottomUL">
+        <li>
+          <BsFacebook /> Facebook
+        </li>
+        <li>
+          <BsInstagram /> Instagram
+        </li>
       </ul>
     </div>
   );
