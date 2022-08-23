5db8ca5d1a370ca4a6c173e65d9d9dd14c09fece
austincomstock <austinjcomstock@gmail.com>
2022-08-23 11:39:45 -0600

    Added list & react-icons

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,41 @@
 import React from "react";
 import "./About.css";
 
+// REACT-ICONS IMPORTS
+import { MdSecurity } from "react-icons/md";
+import { GiFogLight, GiSpring, GiTowTruck } from "react-icons/gi";
+import { TbEngine, TbSitemap } from "react-icons/tb";
+
 export const About = () => {
   return (
-    <div>
-      <h1 className="Sec1">Automotive Upgrades</h1>
+    <div className="About1">
+      <h3 className="AboutAU">Automotive Upgrades</h3>
+      <ul className="AboutUL">
+        <li className="AboutLI">
+          <MdSecurity />
+          Interior & Exterior Protection
+        </li>
+        <li className="AboutLI">
+          <GiFogLight />
+          Lighting
+        </li>
+        <li className="AboutLI">
+          <GiSpring />
+          Suspension & Lift Kits
+        </li>
+        <li className="AboutLI">
+          <GiTowTruck />
+          Winches & Recovery
+        </li>
+        <li className="AboutLI">
+          <TbEngine />
+          Performance
+        </li>
+        <li className="AboutLI">
+          <TbSitemap />
+          All Products
+        </li>
+      </ul>
     </div>
   );
 };
