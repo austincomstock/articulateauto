e7c614d7e7a067e28351c0f906ec3ba5220fe2ff
austincomstock <austinjcomstock@gmail.com>
2022-08-29 16:25:54 -0600

    Created ShopVehicle Make, Model & Year components

---
+++ src/components/ShopVehicleMake/ShopVehicleMake.jsx
@@ -0,0 +1,40 @@
+import { React, useState } from "react";
+
+export const ShopVehicleMake = () => {
+  const vehicleMake = [
+    { label: "--Select Make--", value: "--Select Make--" },
+    { label: "Toyota", value: "Toyota" },
+    { label: "Ford", value: "Ford" },
+    { label: "Ram", value: "Ram" },
+    { label: "Chevrolet", value: "Chevrolet" },
+    { label: "GMC", value: "GMC" },
+    { label: "Hyundai", value: "Hyundai" },
+    { label: "Volkswagon", value: "Volkswagon" },
+  ];
+
+  const [value, setValue] = useState("");
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <div className="ShopVehicleMake1">
+      {/* FORM */}
+      <form action="">
+        <label>
+          <select value={value} onChange={handleChange}>
+            {vehicleMake.map((vehicleMake, index) => (
+              <>
+                <option key={index} value={vehicleMake.value}>
+                  {vehicleMake.label}
+                </option>
+              </>
+            ))}
+          </select>
+        </label>
+        <p>{value}</p>
+      </form>
+    </div>
+  );
+};
---
+++ src/components/ShopVehicleModel/ShopVehicleModel.jsx
@@ -0,0 +1,41 @@
+import { React, useState } from "react";
+
+export const ShopVehicleModel = () => {
+  const vehicleModel = [
+    { label: "--Select Model--", value: "--Select Model--" },
+    { label: "Tacoma", value: "Tacoma" },
+    { label: "F-150", value: "F-150" },
+    { label: "TRX 1500", value: "TRX 1500" },
+    { label: "Silverado", value: "Silverado" },
+    { label: "Sierra 1500", value: "Sierra 1500" },
+    { label: "Tucson", value: "Tucson" },
+    { label: "Jetta", value: "Jetta" },
+  ];
+
+  const [value, setValue] = useState("");
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <div className="ShopVehicleModel1">
+      {/* FORM */}
+      <form>
+        <label>
+          {/* Shop By Vehicle Model: */}
+          <select value={value} onChange={handleChange}>
+            {vehicleModel.map((vehicleModel, index) => (
+              <>
+                <option key={index} value={vehicleModel.value}>
+                  {vehicleModel.label}
+                </option>
+              </>
+            ))}
+          </select>
+        </label>
+        <p>{value}</p>
+      </form>
+    </div>
+  );
+};
