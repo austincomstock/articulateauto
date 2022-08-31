f2635e6741354873ec6986a96e0872bb2b4c5f4c
austincomstock <austinjcomstock@gmail.com>
2022-08-31 09:56:14 -0600

    Added div & updated className, removed h5

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -9,24 +9,24 @@ export const Contact = () => {
   return (
     <div className="Contact1">
       <h3 className="ContactH3">Contact & Connect With Us</h3>
-      <h5 className="ContactH5">Contact</h5>
-      <ul className="ContactUL">
-        <li>
-          <BsTelephone /> 800-000-000
-        </li>
-        <li>
-          <MdOutlineMail /> articulateauto@mail.placeholder
-        </li>
-      </ul>
-      <h5 className="ContactH5">Connect</h5>
-      <ul className="ContactUL">
-        <li>
-          <BsFacebook /> Facebook
-        </li>
-        <li>
-          <BsInstagram /> Instagram
-        </li>
-      </ul>
+      <div className="Contact2">
+        <ul className="ContactConnectUL">
+          <li>
+            <BsFacebook /> Facebook
+          </li>
+          <li>
+            <BsInstagram /> Instagram
+          </li>
+        </ul>
+        <ul className="ContactConnectUL">
+          <li>
+            <BsTelephone /> 800-000-000
+          </li>
+          <li>
+            <MdOutlineMail /> articulateauto@mail
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
