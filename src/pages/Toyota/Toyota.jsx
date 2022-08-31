8de68a432476dbd7d449677f9ead4625ac317974
austincomstock <austinjcomstock@gmail.com>
2022-08-31 09:47:56 -0600

    Added & updated images

--- src/pages/Toyota/Toyota.jsx
+++ src/pages/Toyota/Toyota.jsx
@@ -5,6 +5,8 @@ import { Link, Outlet } from "react-router-dom"; // REACT-ROUTER DOCUMENTATION h
 // IMAGES
 import Taco1Img from "./imgTacoma.jpg";
 import Runner1Img from "./img4runner.jpg";
+import Tundra1Img from "./imgTundra.jpg";
+import FJ1Img from "./imgFJ.jpg";
 
 export const Toyota = () => {
   return (
@@ -17,6 +19,10 @@ export const Toyota = () => {
         <img className="ToyImg1" src={Taco1Img} alt="Toyota Tacoma" />
         4-Runner
         <img className="ToyImg1" src={Runner1Img} alt="Toyota 4-Runner" />
+        Tundra
+        <img className="ToyImg1" src={Tundra1Img} alt="Toyota Tundra" />
+        FJ
+        <img className="ToyImg1" src={FJ1Img} alt="Toyota FJ" />
       </div>
     </div>
   );
