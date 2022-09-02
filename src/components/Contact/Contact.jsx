667034f2c1a00231d941cf0a1df05798c749ac5d
austincomstock <austinjcomstock@gmail.com>
2022-09-02 10:38:18 -0600

    Removed div, updated text, added FB href link

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -8,33 +8,36 @@ import { MdOutlineMail } from "react-icons/md";
 export const Contact = () => {
   return (
     <div className="Contact1">
-      <h3 className="ContactH3">Contact & Connect With Us</h3>
-      <div className="Contact2">
-        <ul className="ContactConnectUL">
-          <li>
+      <h3 className="ContactH3">Connect With Us</h3>
+
+      <ul className="ContactConnectUL">
+        <li>
+          <a
+            href="https://www.facebook.com/ArticulateAuto"
+            target="_blank"
+            rel="noreferrer"
+            className="HLink"
+          >
             <BsFacebook /> Facebook
-          </li>
-          <li>
-            <BsInstagram />{" "}
-            <a
-              href="https://www.instagram.com/articulate_auto/"
-              target="_blank"
-              rel="noreferrer"
-              className="HLink"
-            >
-              Instagram
-            </a>
-          </li>
-        </ul>
-        <ul className="ContactConnectUL">
-          <li>
-            <BsTelephone /> 800-000-000
-          </li>
-          <li>
-            <MdOutlineMail /> info.articulateauto@gmail.com
-          </li>
-        </ul>
-      </div>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.instagram.com/articulate_auto/"
+            target="_blank"
+            rel="noreferrer"
+            className="HLink"
+          >
+            <BsInstagram /> Instagram
+          </a>
+        </li>
+        <li>
+          <BsTelephone /> 800-000-000
+        </li>
+        <li>
+          <MdOutlineMail /> info.articulateauto@gmail.com
+        </li>
+      </ul>
     </div>
   );
 };
