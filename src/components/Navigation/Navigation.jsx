88a9bcd6338c08674fc82120330879d76a03547b
austincomstock <austinjcomstock@gmail.com>
2022-12-06 12:38:32 -0700

    Added comments to Nav class & style

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,27 +1,21 @@
 import React from "react";
 import "./Navigation.css";
 import { BsCart3 } from "react-icons/bs";
-import { TbSitemap } from "react-icons/tb";
 import { VscAccount } from "react-icons/vsc";
-import { ProductsDropdown } from "../Dropdown/ProductsDropdown";
 import { AccountDropdown } from "../Dropdown/AccountDropdown";
 import { CartDropdown } from "../Dropdown/CartDropdown";
 
 export const Navigation = () => {
   return (
-    <div className="Nav1">
-      {/* LIST */}
-      <ul className="NavUL">
-        <li className="NavLI">
-          <ProductsDropdown /> <TbSitemap />
-        </li>
-        <li className="NavLI">
-          <AccountDropdown /> <VscAccount />
-        </li>
-        <li className="NavLI">
-          <CartDropdown /> <BsCart3 />
-        </li>
-      </ul>
-    </div>
+    // <div className="Nav1">
+    <ul className="NavUL">
+      <li className="NavLI">
+        <VscAccount /> <AccountDropdown />
+      </li>
+      <li className="NavLI">
+        <BsCart3 /> <CartDropdown />
+      </li>
+    </ul>
+    // </div>
   );
 };
