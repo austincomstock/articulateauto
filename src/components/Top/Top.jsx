88ce60457d9a32d99baf9d60a2e218382a7b640c
austincomstock <austinjcomstock@gmail.com>
2022-12-06 12:32:33 -0700

    Added Navigation component, & Top2 class/style

--- src/components/Top/Top.jsx
+++ src/components/Top/Top.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import "./Top.css";
 import ArtLogo from "../Images/imgArticulate2.jpg";
 import { Search } from "../Search/Search";
+import { Navigation } from "../Navigation/Navigation";
 
 export const Top = () => {
   return (
-    <div className="Top1">
-      <h1 className="TopH1">Articulate Auto</h1>
-      <img className="TopImg" src={ArtLogo} alt="Articulate Logo" />
-      <Search />
-    </div>
+    <>
+      <div className="Top1">
+        <div className="Top2">
+          <h1 className="TopH1">Articulate Auto</h1>
+          <img className="TopImg" src={ArtLogo} alt="Articulate Logo" />
+        </div>
+        <Navigation />
+        <Search />
+      </div>
+    </>
   );
 };
